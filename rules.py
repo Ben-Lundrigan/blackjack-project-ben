f1d441d96cafbdbdcfb2682f9b80c0e54a0d08e6
Ben-Lundrigan <153955502+Ben-Lundrigan@users.noreply.github.com>
2025-12-02 22:05:59 +0000

    Created validate_bet() and constants for min/max bet

--- rules.py
+++ rules.py
@@ -1,1 +1,13 @@
 # Rules module
+
+MIN_BET = 5
+MAX_BET = 1000
+
+def validate_bet(bet, money):
+    if bet < MIN_BET:
+        return False
+    if bet > MAX_BET:
+        return False
+    if bet > money:
+        return False
+    return True
