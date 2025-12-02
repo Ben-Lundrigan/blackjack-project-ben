2a8f13ea8f42dcfecc782c683775baf2647adca2
Ben-Lundrigan <153955502+Ben-Lundrigan@users.noreply.github.com>
2025-12-02 20:52:37 +0000

    Added simple read/write functions for money file

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 # Money file module
 
 def read_money():
-    pass
+    with open("money.txt") as file:
+        return float(file.readline())
 
 def write_money():
-    pass
+    with open("money.txt", "w") as file:
+        file.write(str(money))
