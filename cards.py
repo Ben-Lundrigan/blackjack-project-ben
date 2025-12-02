e902146b73e2dd835d2599af59ab19b443b18203
Ben-Lundrigan <153955502+Ben-Lundrigan@users.noreply.github.com>
2025-12-02 21:03:42 +0000

    Added create_deck()

--- cards.py
+++ cards.py
@@ -1,4 +1,16 @@
 # Cards module
+import random
 
 def create_deck():
-    pass
+    deck = []
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    ranks = [
+        ("Ace", 11), ("2", 2), ("3", 3), ("4", 4), ("5", 5),
++        ("6", 6), ("7", 7), ("8", 8), ("9", 9),
++        ("10", 10), ("Jack", 10), ("Queen", 10), ("King", 10)
+    ]
+    for suit in suits:
+        for rank, value in ranks:
+            deck.append([suit, rank, value])
+    random.shuffle(deck)
+    return deck
