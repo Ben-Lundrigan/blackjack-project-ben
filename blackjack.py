293ea60f2ec7894f5d75c8423a92bb8137dd9d48
Ben-Lundrigan <153955502+Ben-Lundrigan@users.noreply.github.com>
2025-12-03 16:15:02 +0000

    Added basic game loop

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,7 @@
 # Main module
+from cards import create_Deck, deal_card, show_cards, hand_total
+from rules import validate_bet
+import db
 
 def display_title():
     print("BLACKJACK!")
@@ -6,7 +9,29 @@ def display_title():
     print()
 
 def main():
-    pass
+    money = db.read_money()
+
+    while True:
+        print(f"Money: {money}")
+        bet = float(input("Bet amount: "))
+
+        if not validate_bet(bet, money):
+            print("Invalid bet.")
+            print()
+            continue
+
+        deck = create_deck()
+        player = []
+        dealer = []
+
+        deal_card(deck, player)
+        deal_card(deck, dealer)
+        deal_card(deck, player)
+        deal_card(deck, dealer)
+
+        show_cards(f"YOUR CARDS: {player}")
+
+        # Hit/Stand 
 
 if __name__ == "__main__":
     main()
