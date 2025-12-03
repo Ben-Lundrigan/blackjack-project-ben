189ebe64cac9bd1eaae894a4c84147ca354eb169
Ben-Lundrigan <153955502+Ben-Lundrigan@users.noreply.github.com>
2025-12-03 19:41:51 +0000

    Added again loop, point totals, chip buying

--- blackjack.py
+++ blackjack.py
@@ -14,8 +14,32 @@ def main():
     display_title()
 
     while True:
+        if money < 5:
+            print(f"Your balance is too low to play. (Balance: {money})")
+            buy = input("Do you want to buy more chips? (y/n): ").lower()
+            if buy == "y":
+                try:
+                    add_amount = float(input("Enter amount to add (min $5): "))
+                    if add_amount >= 5:
+                        money += add_amount
+                        db.write_money(money)
+                        print(f"Money: {money}\n")
+                    else:
+                        print("Amount must be at least $5.\n")
+                        continue
+                except ValueError:
+                    print("Please enter a valid number.\n")
+                    continue
+            else:
+                print("Come back soon!\nBye!")
+                break
         print(f"Money: {money}")
-        bet = float(input("Bet amount: "))
+        try:
+            bet = float(input("Bet amount: "))
+            print()
+        except ValueError:
+            print("Invalid input. Enter a number.\n")
+            continue
 
         if not validate_bet(bet, money):
             print("Invalid bet.")
@@ -33,12 +57,14 @@ def main():
 
         print("DEALERS'S SHOW CARD:")
         print(f"{dealer[0][1]} of {dealer[0][0]}")
+        print()
         show_cards("YOUR CARDS:", player)
 
         # Hit/Stand
         while True:
             choice = input("Hit or stand? (hit/stand): ")
             if choice == "hit":
+                print()
                 deal_card(deck, player)
                 show_cards("YOUR CARDS:", player)
                 if hand_total(player) > 21:
@@ -49,6 +75,7 @@ def main():
                 break
 
         # Dealer turn
+        print()
         dealer_hit_to_17(deck, dealer)
         show_cards("DEALER CARDS:", dealer)
 
@@ -62,15 +89,19 @@ def main():
         if dealer_total > 21:
             print("Dealer busts. You win!")
             money += bet
+            print(f"Money: {money}")
         elif player_total > dealer_total:
             print("You win!")
             money += bet
+            print(f"Money: {money}")
         elif player_total < dealer_total:
             print("Sorry. You lose.")
             money -= bet
+            print(f"Money: {money}")
 
         db.write_money(money)
         
+        print()
         again = input("Play again? (y/n): ")
         print()
         if again.lower() != "y":
