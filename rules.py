4438096b70100ba7471d74f7077eb81dc853ff76
Ben-Lundrigan <153955502+Ben-Lundrigan@users.noreply.github.com>
2025-12-03 16:04:52 +0000

    Added dealer_hit_to_17() and blackjack payout

--- rules.py
+++ rules.py
@@ -1,4 +1,5 @@
 # Rules module
+from cards import deal_card, hand_total
 
 MIN_BET = 5
 MAX_BET = 1000
@@ -11,3 +12,10 @@ def validate_bet(bet, money):
     if bet > money:
         return False
     return True
+
+def dealer_hit_to_17(deck, dealer_hand):
+    while hand_total(dealer_hand) < 17:
+        deal_card(deck, dealer_hand)
+
+def blackjack_payout(bet):
+    return round(bet * 1.5, 2)
